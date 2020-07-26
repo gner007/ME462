c288d6e14cca2649e80d3cb67d02681f2cd7f483
Güner Can Dündar <53517530+gner007@users.noreply.github.com>
2020-07-26 16:31:27 +0300

    Add files via upload
    bat file for scripting meshroom

---
+++ run.bat
@@ -0,0 +1,4 @@
+call conda activate base
+pause
+python "C:\Users\guner\OneDrive\Documents\cifcif\run_alicevision\run_alicevision.py" "C:\Users\guner\OneDrive\Documents\cifcif\Copper plastic\trials" "C:\Users\guner\OneDrive\Documents\cifcif\Copper plastic" "C:\Meshroom-2019.2.0\aliceVision\bin" 14 runall
+pause
